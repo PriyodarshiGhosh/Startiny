359ee5c0188163269981962540679a08226a2322
Aarush Aggarwal <91485103+iam4tart@users.noreply.github.com>
2022-11-05 22:02:27 +0530

    Update pred.py

--- demo/pred.py
+++ demo/pred.py
@@ -7,8 +7,9 @@ from sklearn import model_selection
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+
 def prediction(num1, num2, num3,num4,num5,num6, num7, num8, num9, num10, num11, num12, num13, num14, num15, num16, num17, num18, num19, num20, num21,num22,num23,num24,num25,num26):
-    d = pd.read_csv("/Users/priyodarshighosh/demo/startup data.csv")
+    d = pd.read_csv("demo\startup_data.csv")
     d['status'] = np.where(d['status'] == 'closed', 0,1)
     d = d.drop(["Unnamed: 0", "Unnamed: 6", "labels","closed_at", "id"], axis = 1)
     d['age_first_milestone_year'] = d['age_first_milestone_year'].fillna(method="ffill")
@@ -20,11 +21,13 @@ def prediction(num1, num2, num3,num4,num5,num6, num7, num8, num9, num10, num11, 
                'funding_total_usd', 'is_software', 'is_mobile', 'is_consulting', 'is_biotech', 'is_web',
                'is_gamesvideo', 'is_othercategory', 'is_TX', 'has_VC', 'is_ecommerce', 'has_angel', 
                'age_first_funding_year']]
-    scale= StandardScaler()
-    scaled_data = scale.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(scaled_data, Y, test_size=0.20, random_state=7)
-    model = RandomForestClassifier(random_state = 7)
+
+    # scale= StandardScaler()
+    # scaled_data = scale.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=7)
+    model = RandomForestClassifier(max_depth=6, max_features=18, n_estimators=200,random_state=7)
     model.fit(X_train, y_train)
+
     data = {
   'milestones': num1, 
   'is_top500': num2, 
@@ -54,6 +57,7 @@ def prediction(num1, num2, num3,num4,num5,num6, num7, num8, num9, num10, num11, 
   'age_first_funding_year': num26
 }
     df = pd.DataFrame([list(data.values())])
-    scale= StandardScaler()
-    scaled_data = scale.fit_transform(df)
-    return model.predict(df)
+    y_pred1 = model.predict(df)
+    # scale = StandardScaler()
+    # scaled_data = scale.fit_transform(df)
+    return y_pred1
